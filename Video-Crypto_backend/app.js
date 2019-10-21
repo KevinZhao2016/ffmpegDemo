300f5e62dcf964a5d93824ba37105f0af9cff01d
occulticplus@outlook.com <occulticplus@outlook.com>
2019-10-21 12:02:11 +0800

    somegthing

--- Video-Crypto_backend/app.js
+++ Video-Crypto_backend/app.js
@@ -16,13 +16,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit : '100mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); //这个表示任意域名都可以访问，这样写不能携带cookie了。
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1'); //这样写，只有www.baidu.com 可以访问。
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');//设置方法
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use('/api', api);
 
@@ -30,6 +39,7 @@ app.use('/api', api);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
