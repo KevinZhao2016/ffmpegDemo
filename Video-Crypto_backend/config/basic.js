bb0b8e06adae1a828171db24279ff30ecee10177
kevin <0321zyw@sina.com>
2019-11-20 19:45:48 +0800

    html

--- Video-Crypto_backend/config/basic.js
+++ Video-Crypto_backend/config/basic.js
@@ -1,4 +1,4 @@
 module.exports = {
     privateKeyList : ["5K463ynhZoCDDa4RDcr63cUwWLTnKqmdcoTKTHBjqoKfv4u5V7p"],
-    relative_path: "/Users/kevin/Desktop/ffmpegDemo/web/video"
+    relative_path: "/Users/kevin/Desktop/密码学竞赛/ffmpegDemo/web/video"
 }
